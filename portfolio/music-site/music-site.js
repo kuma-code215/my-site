6d5a1c50394616a6fdb6cb33aacea3755c710a5b
haruki <paponta01@gmail.com>
2026-03-15 06:37:32 +0900

    Fix music-site background to golden harp and remove moving circles

--- portfolio/music-site/music-site.js
+++ portfolio/music-site/music-site.js
@@ -9,7 +9,6 @@
   }
 
   setupReveal(revealItems);
-  setupPointerLight();
 
   function setupReveal(elements) {
     if (!elements.length) {
@@ -33,29 +32,6 @@
     elements.forEach((element, index) => {
       element.style.transitionDelay = `${Math.min(index * 80, 260)}ms`;
       observer.observe(element);
-    });
-  }
-
-  function setupPointerLight() {
-    let rafId = 0;
-
-    const update = (x, y) => {
-      if (rafId) {
-        cancelAnimationFrame(rafId);
-      }
-
-      rafId = requestAnimationFrame(() => {
-        const xp = `${x}%`;
-        const yp = `${y}%`;
-        document.documentElement.style.setProperty("--mouse-x", xp);
-        document.documentElement.style.setProperty("--mouse-y", yp);
-      });
-    };
-
-    window.addEventListener("pointermove", (event) => {
-      const x = (event.clientX / window.innerWidth) * 100;
-      const y = (event.clientY / window.innerHeight) * 100;
-      update(x, y);
     });
   }
 })();
